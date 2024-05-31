b8ed95f79a88e6148b63115c6c462197a9212abe
icarus1411 <108058720+Icarus1411@users.noreply.github.com>
2024-05-31 20:22:48 +0800

    rollback Wrong merge

--- IdeaProjects/SJTU-Hire/frontend/src/utils/message.js
+++
@@ -1,9 +0,0 @@
-export async function handleBaseApiResponse(res, messageApi, onSuccess, onFail) {
-    if (res.ok) {
-        await messageApi.success(res.message, 0.5);
-        onSuccess?.();
-    } else {
-        await messageApi.error(res.message, 0.5);
-        onFail?.();
-    }
-}
--- IdeaProjects/SJTU-Hire/frontend/src/utils/refresh.jsx
+++
@@ -1,4 +0,0 @@
-// 重新加载当前页面
-export const refreshPage = () => {
-    window.location.reload();
-}
